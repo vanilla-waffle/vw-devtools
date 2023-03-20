baafe45a93e5fab95ba864c482959cf58a1c7ef3
khanZhomart <jtalgatulu@yandex.kz>
2023-03-20 15:48:07 +0600

    #34 Запуск API с базой

---
+++ api/README.txt
@@ -0,0 +1,2 @@
+[Requires an env file to run]
+docker-compose --env-file .env up
---
+++ api/insert.py
@@ -0,0 +1,31 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = "localhost"
+DB_PORT = 3306
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASSWORD")
+
+def insert_data(file_path, table_name):
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS
+    )
+    cur = conn.cursor()
+    with open(file_path, "r") as f:
+        next(f)  # skip header row
+        cur.copy_from(f, table_name, sep=",")
+        conn.commit()
+        cur.close()
+        conn.close()
+        print(f"Inserted data from {file_path} into {table_name} table")
+
+insert_data("users.csv", "users")
+insert_data("post.csv", "post")                                                               
