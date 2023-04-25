29418ebd574af08cfb134b6cb858c2498d0dc668
khanZhomart <jtalgatulu@yandex.kz>
2023-04-25 17:30:30 +0600

    Доработка команды npm run unzip

--- latex/src/unzip.js
+++ latex/src/unzip.js
@@ -4,6 +4,6 @@ import { execSync } from 'child_process'
 dotenv.config()
 
 const path = process.env.REPOSITORY
-const output_path = '../output/'
+const output_path = './output/'
 
-execSync(`7z x ${output_path}/vw-latex.zip -o${path}`)
+execSync(`7z x -y ${output_path}vw-latex.zip -o${path}`)
